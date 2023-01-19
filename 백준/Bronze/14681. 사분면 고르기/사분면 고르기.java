be0c75ccac2eb6255ab935732e98859eda9b1f56
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-01-19 20:59:05 +0900

    [Bronze V] Title: 사분면 고르기, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		int result = 1;
+		if(x>0 && y>0) {
+			result = 1;
+		}else if(x<0 && y>0) {
+			result = 2;
+		}else if(x<0 && y<0) {
+			result = 3;
+		}else if(x>0 && y<0) {
+			result = 4;
+		}
+		System.out.println(result);
+		
+	}
+}
