2efc5e444df2e158e5491e7010d3ce8dfc13f07a
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-01-21 14:19:43 +0900

    [Bronze V] Title: A+B - 3, Time: 148 ms, Memory: 15816 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		int n = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<n; i++) {
+			String [] str = br.readLine().split(" ");
+			int a = Integer.parseInt(str[0]);
+			int b = Integer.parseInt(str[1]);
+			sb.append(a+b+"\n");
+		}
+		System.out.println(sb);
+	}
+}	
