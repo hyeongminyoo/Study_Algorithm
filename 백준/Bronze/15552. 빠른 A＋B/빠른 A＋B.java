dad801a65c0b367ecab5f2e28b6ee05ad53363d0
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-01-22 12:20:58 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 808 ms, Memory: 168208 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.java
@@ -1,18 +1,23 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		long t = Long.parseLong(br.readLine());
-		for(long i=0; i<t; i++) {
-			String [] str = br.readLine().split(" ");
-			bw.write(Integer.parseInt(str[0])+Integer.parseInt(str[1])+"\n");
+ 
+	public static void main(String[] args) throws Exception {
+ 
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+ 
+		for (int i = 0; i < N; i++) {		
+			String str = br.readLine();
+			int target = str.indexOf(" ");
+			int result = Integer.parseInt(str.substring(0,target)) + Integer.parseInt(str.substring(target + 1));		
+			sb.append(result+"\n");
 		}
 		
-		bw.flush();
-		bw.close();
-		
-	}	
+		br.close();
+		System.out.print(sb);
+    }
 }
