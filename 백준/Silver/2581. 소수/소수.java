0aef178fa12d8b39b85960c9bc39709cb01aa142
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-02-08 16:08:38 +0900

    [Silver V] Title: 소수, Time: 124 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.java
+++ 백준/Silver/2581. 소수/소수.java
@@ -8,46 +8,45 @@ public class Main {
 		int m = Integer.parseInt(br.readLine());
 		int n = Integer.parseInt(br.readLine());
 		
+		boolean [] prime = new boolean[n+1];
+		getPrime(prime);
+		
 		int sum = 0;
-		int min = 0;
-			
-		for(int i=m; i<n+1; i++) {
-			
-			boolean check = true;
-            
-            if(i == 1) {
-				check = false;
-			}
-            
-			for(int j=2; j<=Math.sqrt(i); j++) {
-				if(i%j ==0 ) {
-					check = false;
-					break;
-				}
-			}
-			
-			
-			
-			if(check) {
-				if(sum == 0) {
-					sum += i;
+		int min = Integer.MAX_VALUE;
+		
+		for(int i=m; i<= n; i++) {
+			if(prime[i] == false) {
+				sum+=i;
+				if(min == Integer.MAX_VALUE) {
 					min = i;
-				}else {
-					sum += i;
 				}
 			}
-			
 		}
 		
 		
-		if(sum==0) {
+		if(sum == 0) {
 			System.out.println(-1);
 		}else {
 			System.out.println(sum);
 			System.out.println(min);
 		}
 		
+	}
+	
+	
+	public static void getPrime(boolean [] prime) {
+		prime[0] = true;
+		prime[1] = true;
+		
+		for(int i=2; i<= Math.sqrt(prime.length); i++) {
+			if(prime[i]) continue;
+			for(int j=i*i; j<prime.length; j += i) {
+				prime[j] = true;
+			}
+		}
 		
 	}
+	
+	
 		
 }
