ab503221bc7555c4a5a402a4829391f25fda5f48
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-02-24 17:40:07 +0900

    [Bronze II] Title: 분해합, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -5,11 +5,13 @@ public class Main {
 	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		 
-		int n = Integer.parseInt(br.readLine());
+		
+		String str = br.readLine();
+		int len = str.length();
+		int n = Integer.parseInt(str);
 		
 		int result = 0;
-		for(int i=1; i<n; i++) {
+		for(int i=(n-(len*9)); i<n; i++) {
 			int num = i;
 			int sum = 0;
 			while(num !=0) {
