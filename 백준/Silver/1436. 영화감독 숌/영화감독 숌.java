925e4e65a6838f56e8a622a05b677ea5a2ef4bfe
hyeongmin <108776158+hyeongminyoo@users.noreply.github.com>
2023-02-27 17:25:13 +0900

    [Silver V] Title: 영화감독 숌, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -9,23 +9,83 @@ public class Main {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		int check = 666;
-		
-		int count = 1;
-		
-		while(count != n) {
-			check++;
-			if(String.valueOf(check).contains("666")) {
-				count++;
-			}
+		if(n>1) {
+			func(n);
+		}else {
+			System.out.println(666);
 		}
-		
-		System.out.println(check);
 		
 		
 		
 		
 	}
+	
+	public static void func(int n) {
+		
+		int prev_digit = 0;
+		int count = 1;
+		int num = 0;
+		
+		while(true) {
+			if((prev_digit % 10000)/10 == 666 && prev_digit % 10 !=6) {
+				for(int i=0; i<100; i++) {
+					if(count == n) {
+						System.out.println(prev_digit * 1000 + num);
+						return;
+					}
+					
+					num++;
+					count++;
+				}
+				prev_digit++;
+			
+			}else if(prev_digit%1000 == 666) {
+				num = 0;
+				for(int i=0; i<1000; i++) {
+					if(count ==n) {
+						System.out.println(prev_digit*1000+num);
+						return;
+					}
+					count++;
+					num++;
+				}
+				prev_digit++;
+			}else if(prev_digit % 100 == 66) {
+				num = 600;
+				for(int i = 0; i<100; i++) {
+					if(count == n) {
+						System.out.println(prev_digit*1000 + num);
+						return;
+					}
+					count++;
+					num++;
+				}
+				prev_digit++;
+			}else if(prev_digit%10 == 6) {
+				num = 660;
+				for(int i=0; i<10; i++) {
+					if(count == n) {
+						System.out.println(prev_digit*1000+num);
+						return;
+					}
+					num++;
+					count++;
+				}
+				prev_digit++;
+			}else {
+				num = 666;
+				if(count == n) {
+					System.out.println(prev_digit*1000+num);
+					return;
+				}
+				count++;
+				prev_digit++;
+			}
+			
+		}
+		
+	}
+	
 	
 	
 	
